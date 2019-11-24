1554a855051585deabba9c6da059b2c2ffc84009
evgeniyamelone <jen-o@yandex.ru>
2019-11-24 21:08:05 +0400

    Removed some output

--- src/main/java/movableInterface/Demo.java
+++ src/main/java/movableInterface/Demo.java
@@ -7,18 +7,14 @@ public class Demo {
 
         MovableRectangle rectangle = new MovableRectangle(topLeft, bottomRight);
         rectangle.print();
-        System.out.print("Move up => ");
         rectangle.moveUp();
         rectangle.print();
         rectangle.setSpeed(-2000, 100);
-        System.out.print("Move down => ");
         rectangle.moveDown();
         rectangle.print();
-        System.out.print("Move right => ");
         rectangle.moveRight();
         rectangle.print();
         rectangle.setSpeed(3000, -1100);
-        System.out.print("Move left => ");
         rectangle.moveRight();
         rectangle.print();
     }
