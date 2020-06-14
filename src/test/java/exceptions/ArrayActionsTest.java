496ce2955c923dcca2612595718f855178629035
evgeniyamaslennikova <jen-o@yandex.ru>
2020-06-14 17:11:00 +0300

    Rename test

--- src/test/java/exceptions/ArrayActionsTest.java
+++ src/test/java/exceptions/ArrayActionsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class ArrayActionsTest {
     @Test
-    void GetElement() {
+    void checkIndex() {
         int arr[] = {1, 2, 3, 4};
         Assertions.assertThat(ArrayActions.checkIndex(arr, 3)).isTrue();
         Assertions.assertThat(ArrayActions.checkIndex(arr, 5)).isFalse();
