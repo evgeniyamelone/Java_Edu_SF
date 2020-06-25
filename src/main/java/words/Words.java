35212f7f92e567cc5c9d11e82240bf2b2c3fee83
evgeniyamaslennikova <jen-o@yandex.ru>
2020-06-25 08:42:11 +0300

    Collect text to List<String> and then use String.join

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,17 +1,20 @@
 package words;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Words {
     public static String matches(String regex, String text) {
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(text);
-            String result = "";
-            while (matcher.find()) {
-                result = result + text.substring(matcher.start(), matcher.end())+" ";
-            }
-            return result.trim();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        List<String> list = new ArrayList<>();
+        while (matcher.find()) {
+            list.add(text.substring(matcher.start(), matcher.end()) + " ");
         }
+        return String.join("", list).trim();
     }
+}
 
